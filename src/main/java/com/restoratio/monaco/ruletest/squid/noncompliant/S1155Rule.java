41f07b34201d61ee90668e101551d3a197788bff
codefix-service-user <vadiraja.k+codefix@codenation.co.in>
2020-05-15 11:51:58 +0000

    CodeFix#9999231#[615907587]

--- src/main/java/com/restoratio/monaco/ruletest/squid/noncompliant/S1155Rule.java
+++ src/main/java/com/restoratio/monaco/ruletest/squid/noncompliant/S1155Rule.java
@@ -8,7 +8,7 @@ public class S1155Rule {
     private final List<String> children = new ArrayList<>();
 
     public String generateChildrenText() {
-        if (children.size() > 0) {
+        if (!children.isEmpty()) {
             return String.format("Has %d children", children.size());
         }
         if (children.size() > 0) {
